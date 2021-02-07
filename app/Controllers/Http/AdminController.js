1b8778f2db93e1beb2f515ab1c0e664414c08753
aldafawnia <aldafawnia88@gmail.com>
2021-02-07 18:24:29 +0000

    admin can register  account

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -1,8 +1,19 @@
 'use strict'
 
+const Admin = use('App/Models/Admin')
+
 class AdminController {
-  register({view}){
+  async register({view}){
     return view.render('adminregister')
+  }
+
+  async processRegister({request,response}){
+    let formData = request.post()
+    let newAdmin = new Admin()
+    newAdmin.username = formData.username
+    newAdmin.password = formData.password
+    await newAdmin.save()
+    response.json(newAdmin)
   }
 }
 
